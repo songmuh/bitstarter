a7c6838ea43819e459675d63b7808383c5c9eee0
songmuh <songmuh@yahoo.com>
2013-07-17 18:07:20 -0700

    Add some content

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/images'));
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("./index.html");
